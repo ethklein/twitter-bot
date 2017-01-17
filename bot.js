cf5313084d37d2dd8125f822b6cc33f0e9a48d63
Ethan Klein <ethanklein@gwu.edu>
2017-01-17 16:19:28 -0500

    twit package setup

---
+++ bot.js
@@ -0,0 +1,11 @@
+console.log('the bot is starting');
+
+var twit = require('twit'); //import twit package
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...' 
+})
+
